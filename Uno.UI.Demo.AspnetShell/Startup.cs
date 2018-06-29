76275da62b00bd19a65273134eed773cfed0f7ba
MarkStega <mark2002@stega.us>
2018-06-29 11:22:54 -0400

    2018-06-29 Update AspNetCore to 2.1.1

--- Uno.UI.Demo.AspnetShell/Startup.cs
+++ Uno.UI.Demo.AspnetShell/Startup.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Rewrite;
 using Microsoft.AspNetCore.StaticFiles;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.WindowsAzure.Storage.Auth;
-using Microsoft.WindowsAzure.Storage.Table;
 
 namespace Uno.UI.Demo.AspnetShell
 {
