94cf69d0c5c109ef72d69932a4a8d4ed0109679b
Jérôme Laban <jerome.laban@nventive.com>
2018-11-21 11:45:56 -0500

    Use navigation view wip

---
+++ src/Uno.Playground.Shared/Controls/GalleryNavigationViewItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Xaml.Controls;
+
+namespace Uno.Playground.Controls
+{
+    public class GalleryNavigationViewItem : NavigationViewItem
+	{
+		public GalleryNavigationViewItem()
+		{
+			DefaultStyleKey = typeof(GalleryNavigationViewItem);
+		}
+    }
+}
