6110ad8fdb5084aa0b84765363f2f328f76b98ae
Jérôme Laban <jerome.laban@nventive.com>
2018-12-13 14:22:44 -0500

    Update Uno, disable binary interop

--- src/Uno.Playground.WASM/Program.cs
+++ src/Uno.Playground.WASM/Program.cs
@@ -8,6 +8,9 @@ namespace Uno.UI.Demo
 
 		public static int Main(string[] args)
 		{
+			Uno.UI.FeatureConfiguration.Interop.ForceJavascriptInterop = true;
+
+			Console.WriteLine("go");
 			_app = new App();
 
 			return 0;
