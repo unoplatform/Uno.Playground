02fc43b68e9c34822e36731c453a27ae11469ff3
Jérôme Laban <jerome.laban@nventive.com>
2018-12-18 21:15:08 -0500

    Fix progress sample memory leak

--- src/Uno.Playground.Shared/Samples/Progress.xaml.cs
+++ src/Uno.Playground.Shared/Samples/Progress.xaml.cs
@@ -19,19 +19,36 @@ namespace Uno.UI.Demo.Samples
 {
 	public sealed partial class Progress : UserControl
 	{
+		private int _ticks = 0;
+		private DispatcherTimer _timer;
+
 		public Progress()
 		{
 			this.InitializeComponent();
 
-			var ticks = 0;
-			var timer = new DispatcherTimer();
-			timer.Interval = TimeSpan.FromMilliseconds(20);
-			timer.Tick += (s, e) =>
-			{
-				ticks++;
-				DataContext = ticks % 100;
-			};
-			timer.Start();
+			Loaded += OnLoaded;
+			Unloaded += OnUnloaded;
+
+			_timer = new DispatcherTimer();
+			_timer.Interval = TimeSpan.FromMilliseconds(20);
+		}
+
+		void OnTick(object s2, object a2)
+		{
+			_ticks++;
+			DataContext = _ticks % 100;
+		}
+
+		private void OnLoaded(object sender, RoutedEventArgs args)
+		{
+			_timer.Tick += OnTick;
+			_timer.Start();
+		}
+
+		private void OnUnloaded(object sender, RoutedEventArgs args)
+		{
+			_timer.Tick -= OnTick;
+			_timer.Stop();
 		}
 	}
 }
