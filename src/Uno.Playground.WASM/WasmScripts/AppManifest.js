82e7284c7ff65c09df7ae7c7a6d935e994f7d2f0
Jérôme Laban <jerome.laban@nventive.com>
2018-12-23 22:22:46 -0500

    Add title name

--- src/Uno.Playground.WASM/WasmScripts/AppManifest.js
+++ src/Uno.Playground.WASM/WasmScripts/AppManifest.js
@@ -1,6 +1,6 @@
 ﻿var UnoAppManifest = {
 
     splashScreenImage: "Assets/SplashScreen.scale-200.png",
-    splashScreenColor: "#fff"
-
+    splashScreenColor: "#fff",
+    displayName: "Uno Gallery - AOT",
 }
