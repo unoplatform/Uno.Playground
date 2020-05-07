618e1fa51c1c3c3b7b291abdcd9403f0a2ae5b8d
Stewart Polsky <stewart.polsky@nventive.com>
2020-05-07 15:56:01 -0400

    Added AutomationID property and added UITest to solution

---
+++ src/Uno.Playground.UITest/AppInitializer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace Uno.Playground.UITest
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+				return ConfigureApp
+					.Android
+					.InstalledApp("com.nventive.uno.ui.demo")
+					.EnableLocalScreenshots()
+					.Debug()
+					.StartApp();
+
+            }
+
+            return ConfigureApp
+				.iOS
+				.StartApp();
+        }
+    }
+}
---
+++ src/Uno.Playground.UITest/Tests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace Uno.Playground.UITest
+{
+    [TestFixture(Platform.Android)]
+    [TestFixture(Platform.iOS)]
+    public class Tests
+    {
+        IApp app;
+        Platform platform;
+
+        public Tests(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform);
+        }
+
+		[Test]
+		public void Smoke()
+		{
+			app.Repl();
+		}
+    }
+}
