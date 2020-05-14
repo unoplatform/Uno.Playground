ca1e9b7ee4bb7270b7b122937cfc419f6e553294
Stewart Polsky <stewart.polsky@nventive.com>
2020-05-14 17:30:38 -0400

    Added BoolToString Converter. CHanged Checkbox text to change depending on if it is checked

---
+++ src/Uno.Playground.Shared/Converters/BoolToStringConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Uno.UI.Demo.Samples;
+using Windows.UI.Xaml.Data;
+using Windows.Web.Syndication;
+
+namespace Uno.UI.Demo.Converters
+{
+	public class BoolToStringConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, string language)
+		{
+			if(value is bool)
+			{
+				if ((bool)value)
+				{
+					return "Is Checked";
+				}
+				else
+				{
+					return "Is Not Checked";
+				}
+			}
+			return "Invalid";
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, string language)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
